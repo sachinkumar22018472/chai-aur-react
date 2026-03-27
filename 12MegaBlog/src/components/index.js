3821c0e6a5738e8971f4ac71da31a77da7a1be9f
Sachin Kumar <sachinkumar22018472@gmail.com>
2026-03-27 23:34:56 +0530

    components

---
+++ 12MegaBlog/src/components/index.js
@@ -0,0 +1,15 @@
+import Header from "./Header/Header";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Footer from "./Footer/Footer";
+
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
